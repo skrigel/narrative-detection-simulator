19d47535e8373e19d7cdc9aaa4a7970de922b324
Sasha Krigel <sasha.krigel@gmail.com>
2025-08-03 18:44:28 +0100

    Update env value

--- frontend/src/api/syntheticService.ts
+++ frontend/src/api/syntheticService.ts
@@ -1,5 +1,6 @@
 import { type NarrativeRequest, type NarrativeResponse, type PlatformNarrativeRequest } from "../types/narrative";
-const API_BASE_URL = import.meta.env.FAST_API_URL || "http://localhost:8000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
+console.log(import.meta.env.VITE_API_URL);
 
 type AnyNarrativeRequest = NarrativeRequest | PlatformNarrativeRequest;
 
